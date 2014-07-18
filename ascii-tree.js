35f06c3f55322534648a20212d0b9e5e56d0e67f
Shuping LIU <lsp.lhh@gmail.com>
2014-07-18 10:04:35 +0800

    add test files

--- ascii-tree.js
+++ ascii-tree.js
@@ -4,38 +4,40 @@ var chars = [String.fromCharCode(9500), String.fromCharCode(9472), String.fromCh
 
 function generate(str) {
     var tree = freetree.parse(str);
-    return generate(tree, true);
+    return _generate(tree, true);
 }
 
-function generate(tree, end) {
+function _generate(tree) {
     var result = '';
     var leadingChars = '';
 
     if (tree.level == 0) {
-        return tree.value;
-    }
-
-    for (i = 1; i <= level - 1; ++i) {
-        if (levels[i]) {
-            leadingChars += '   ';
-        } else {
-            leadingChars += chars[0] + chars[1] + ' ';
-        }
-    }
-
-    if (end) {
-        levels[tree.level] = true;
-        result += chars[2] + chars[1] + ' ' + tree.value;
+	result = tree.value;
     } else {
-        result += chars[0] + chars[1] + ' ' + tree.value;
+	    for (i = 1; i <= tree.level; ++i) {
+		if (levels[i]) {
+		    leadingChars += '   ';
+		} else {
+		    leadingChars += chars[0] + chars[1] + ' ';
+		}
+	    }
+
+	    if (end) {
+		result += '\r\n' + leadingChars + chars[2] + chars[1] + ' ' + tree.value;
+	    } else {
+		result += '\r\n' + leadingChars + chars[0] + chars[1] + ' ' + tree.value;
+	    }
     }
 
-    for (i = 0; i < tree.length - 1; ++i) {
-        result += generate(tree.nodes[i], false);
-    }
+    if (tree.nodes) {
+	    for (i = 0; i < tree.nodes.length - 1; ++i) {
+		result += '\r\n' + _generate(tree.nodes[i]);
+	    }
 
-    if (tree.length > 0) {
-        result += generate(tree.nodes[tree.length - 1], true);
+	    if (tree.nodes.length > 0) {
+	        levels[tree.nodes[tree.length - 1].level] = true;
+		result += '\r\n' + _generate(tree.nodes[tree.length - 1]);
+	    }
     }
 
     return result;
--- test/ascii-tree.test.js
+++ test/ascii-tree.test.js
@@ -1,10 +1,18 @@
 var assert = require('assert');
-var ascii-tree = require('../ascii-tree');
+var asciitree = require('../ascii-tree');
 
 describe('#generate', function() {
     it('should parse a single root tree', function() {
         var tree = '#root';
-        var result = ascii-tree.generate(tree);
+        var result = asciitree.generate(tree);
+	console.log(result);
         assert.ok(result == 'root');
-    })
-})
+    });
+console.log('>>>>>>>>>>>>>>>>>>>');
+    it('should parse 2 level tree', function() {
+ 	var tree = '#root';
+	var result = asciitree.generate(tree);
+	console.log(result);
+	assert.ok(result == 'root');
+    });
+});
