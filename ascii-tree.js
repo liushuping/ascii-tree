9888e65896b12b27c4b60e6dcdad1b0e1a27415a
sliu7 <sliu7@slb.com>
2014-07-20 00:00:18 +0800

    work for n level

--- ascii-tree.js
+++ ascii-tree.js
@@ -1,6 +1,10 @@
 var levels = [];
 var freetree = require('freetree');
-var chars = [String.fromCharCode(9500), String.fromCharCode(9472), String.fromCharCode(9492)]; 
+var chars = [
+    String.fromCharCode(9500),
+    String.fromCharCode(9472),
+    String.fromCharCode(9492),
+    String.fromCharCode(9474)]; 
 
 function generate(str) {
     var tree = freetree.parse(str);
@@ -8,12 +12,19 @@ function generate(str) {
 }
 
 function compose(tree, end) {
+    var i, ret = '\r\n';
+    var c = end ? chars[2] : chars[0];
+
     if (tree.level == 0) {
         return tree.value;
-    } else {
-        var c = end ? chars[2] : chars[0];
-        return '\r\n' + c + chars[1] + ' ' + tree.value;
     }
+
+    for (i = 1; i < tree.level; ++i) {
+        ret += levels[i] ? ' ' : chars[3];
+        ret += '  ';
+    }
+
+    return ret + c + chars[1] + ' ' + tree.value;
 }
 
 function _generate(tree, end) {
@@ -22,8 +33,9 @@ function _generate(tree, end) {
 
     if (tree.nodes) {
         last = tree.nodes.length - 1;
-        tree.nodes.forEach(function(node, index) {
-            result +=  _generate(node, index == last);
+        tree.nodes.forEach(function(subTree, index) {
+            levels[subTree.level] = index == last;
+            result +=  _generate(subTree, index == last);
         });
     }
 
