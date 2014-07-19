075309305ffe55916b0dacd648264e7e74c4a7c1
sliu7 <sliu7@slb.com>
2014-07-19 23:25:48 +0800

    enhance the test file for showing test cases numbers

--- test/ascii-tree.test.js
+++ test/ascii-tree.test.js
@@ -7,18 +7,18 @@ var c3 = String.fromCharCode(9492);
 var inputs = [
     '#root',
     '#root\r\n##level1_1\r\n##level1_2\r\n##level1_3',
-//    '#root\r\n##level1_1\r\n###leve2_1\r\n##level1_2'
+    //    '#root\r\n##level1_1\r\n###leve2_1\r\n##level1_2'
 ];
 
 var outputs = [
     'root',
     'root\r\n' + c1 + c2 + ' level1_1\r\n' + c1 + c2 + ' level1_2\r\n' + c3 + c2 + ' level1_3',
-//    'root'
+    //    'root'
 ];
 
 describe('#generate', function() {
-    it('should parse trees for all inputs', function() {
-        inputs.forEach(function(input, index) {
+    inputs.forEach(function(input, index) {
+        it('should parse trees for all inputs', function() {
             var result = asciitree.generate(input);
             console.log(result);
             assert.ok(result == outputs[index]);
