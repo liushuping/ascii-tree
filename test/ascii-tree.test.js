b633cde36dd5052570ae1016b9fd220a8c1698b2
sliu7 <sliu7@slb.com>
2014-07-20 00:17:41 +0800

    fix the failed test case

--- test/ascii-tree.test.js
+++ test/ascii-tree.test.js
@@ -3,25 +3,25 @@ var asciitree = require('../ascii-tree');
 var c1 = String.fromCharCode(9500);
 var c2 = String.fromCharCode(9472);
 var c3 = String.fromCharCode(9492);
+var c4 = String.fromCharCode(9474);
 
 var inputs = [
     '#root',
     '#root\r\n##level1_1\r\n##level1_2\r\n##level1_3',
-    '#root\r\n##level1_1\r\n###leve2_1\r\n###level2_2\r\n##level1_2'
+    '#root\r\n##level1_1\r\n###level2_1\r\n##level1_2'
 ];
 
 var outputs = [
     'root',
     'root\r\n' + c1 + c2 + ' level1_1\r\n' + c1 + c2 + ' level1_2\r\n' + c3 + c2 + ' level1_3',
-    'root'
+    'root\r\n' + c1 + c2 + ' level1_1\r\n' + c4 + '  ' + c3 + c2 + ' level2_1\r\n' + c3 + c2 + ' level1_2'
 ];
 
 describe('#generate', function() {
     inputs.forEach(function(input, index) {
-        it('should parse trees for all inputs', function() {
+        it('should parse trees for all input ' + index, function() {
             var result = asciitree.generate(input);
-            console.log(result);
-            assert.ok(result == outputs[index]);
+            assert.ok(result.trim() == outputs[index].trim());
         });
     });
 });
