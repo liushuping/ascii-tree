8e1a4068e2334b77f85d5caf43a7107ecfdfccf2
sliu7 <sliu7@slb.com>
2014-07-19 22:59:51 +0800

    work for 1 evel tree

--- ascii-tree.js
+++ ascii-tree.js
@@ -7,37 +7,26 @@ function generate(str) {
     return _generate(tree, true);
 }
 
-function _generate(tree) {
+function _generate(tree, end) {
+    var last;
     var result = '';
     var leadingChars = '';
 
     if (tree.level == 0) {
-	result = tree.value;
+        result = tree.value;
     } else {
-	    for (i = 1; i <= tree.level; ++i) {
-		if (levels[i]) {
-		    leadingChars += '   ';
-		} else {
-		    leadingChars += chars[0] + chars[1] + ' ';
-		}
-	    }
-
-	    if (end) {
-		result += '\r\n' + leadingChars + chars[2] + chars[1] + ' ' + tree.value;
-	    } else {
-		result += '\r\n' + leadingChars + chars[0] + chars[1] + ' ' + tree.value;
-	    }
+        if (end) {
+            result += '\r\n' + leadingChars + chars[2] + chars[1] + ' ' + tree.value;
+        } else {
+            result += '\r\n' + leadingChars + chars[0] + chars[1] + ' ' + tree.value;
+        }
     }
 
     if (tree.nodes) {
-	    for (i = 0; i < tree.nodes.length - 1; ++i) {
-		result += '\r\n' + _generate(tree.nodes[i]);
-	    }
-
-	    if (tree.nodes.length > 0) {
-	        levels[tree.nodes[tree.length - 1].level] = true;
-		result += '\r\n' + _generate(tree.nodes[tree.length - 1]);
-	    }
+        last = tree.nodes.length - 1;
+        tree.nodes.forEach(function(node, index) {
+            result +=  _generate(node, index == last);
+        });
     }
 
     return result;
--- test/ascii-tree.test.js
+++ test/ascii-tree.test.js
@@ -1,18 +1,24 @@
 var assert = require('assert');
 var asciitree = require('../ascii-tree');
+var c1 = String.fromCharCode(9500);
+var c2 = String.fromCharCode(9472);
+var c3 = String.fromCharCode(9492);
+
+var inputs = [
+    '#root',
+    '#root\r\n##level1_1\r\n##level1_2\r\n##level1_3'
+];
+
+var outputs = [
+    'root',
+    'root\r\n' + c1 + c2 + ' level1_1\r\n' + c1 + c2 + ' level1_2\r\n' + c3 + c2 + ' level1_3'
+];
 
 describe('#generate', function() {
-    it('should parse a single root tree', function() {
-        var tree = '#root';
-        var result = asciitree.generate(tree);
-	console.log(result);
-        assert.ok(result == 'root');
-    });
-console.log('>>>>>>>>>>>>>>>>>>>');
-    it('should parse 2 level tree', function() {
- 	var tree = '#root';
-	var result = asciitree.generate(tree);
-	console.log(result);
-	assert.ok(result == 'root');
+    it('should parse trees for all inputs', function() {
+        inputs.forEach(function(input, index) {
+            var result = asciitree.generate(input);
+            assert.ok(result == outputs[index]);
+        });
     });
 });
